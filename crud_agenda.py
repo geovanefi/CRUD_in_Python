2a08466b245ffb00d4bfc3500de7b279363db5f5
Queen Developers - By Geovane Felicio <geovanejr@outlook.com>
2023-05-18 23:27:27 -0300

    Finalização do Codigo

--- crud_agenda.py
+++ crud_agenda.py
@@ -2,8 +2,8 @@
 
 import json, os
 
-# Adicona contatos
-def add_contatos():
+
+def adicionar_contato():
 
     nome = input("nome do contato: ")
     telefone = input("numero do telefone: ")
@@ -32,7 +32,7 @@ def add_contatos():
         print("Dados inseridos com SUCESSO")
 
 
-def ver_contatos():
+def listar_contato():
 
     with open("agenda.json", "r") as visualizar:
         itensdata = json.load(visualizar)
@@ -42,7 +42,7 @@ def ver_contatos():
                 print(x, n)
             print("\n")
 
-def delete_contatos():
+def deletar_contato():
     nome = input("nome do contato: ")
 
     with open("agenda.json", "r") as busca_itens:
@@ -55,24 +55,73 @@ def delete_contatos():
                 itensdata1 = json.dump(itensdata, delete)
                 print("Contato deletado com sucesso")
 
-ver_contatos()
+def atualizar_contato():
+    nome = input("nome do contato: ")
+
+    with open("agenda.json", "r") as busca_itens:
+        itensdata = json.load(busca_itens)
+
+        if nome in itensdata:
+            telefone = input("numero do telefone: ")
+            email = input("endereço de e-mail: ")
+            twitter = input("conta do Twitter: ")
+            instagram = input("@ do Instagram: ")
+
+            dicionario = {
+                "Nome: ": nome,
+                "Telefone: ": telefone,
+                "Email: ": email,
+                "Twitter: ": twitter,
+                "Instagram: " : instagram
+            }
+
+            itensdata[nome] = dicionario
+            with open("agenda.json", "w") as insere_contato:
+                json.dump(itensdata, insere_contato)
+                print("Contato inserido com sucesso!")
+
+def pesquisar_contato():
+    nome = input("nome do contato: ")
+    with open("agenda.json", "r") as busca_itens:
+        itensdata = json.load(busca_itens)
+        if nome in itensdata:
+            print(itensdata[nome])
+        else:
+            print("Contato não encontrado")
+            
+def pesquisar_todos_contatos():
+    with open("agenda.json", "r") as busca_itens:
+        itensdata = json.load(busca_itens)
+        print(itensdata)
 
 
 
+def menu():
+    print("\n ====> MENU INICIAR <=====")
+    print("\n 1. Adicionar Contato")
+    print("\n 2. Listar Contatos")
+    print("\n 3. Excluir Contato")
+    print("\n 4. Atualizar Contato")
+    print("\n 5. Pesquisar Contato")
+    print("\n 6. Pesquisar Todos Contatos")
+    print("\n 7. Sair")
+    print("====> digite o numero correspondente. <=====\n")
 
+    selecao = int(input("Escolha a opção: "))
 
+    if selecao == 1:
+        adicionar_contato()
+    elif selecao == 2:
+        listar_contato()
+    elif selecao == 3:
+        deletar_contato()
+    elif selecao == 4:
+        atualizar_contato()
+    elif selecao == 5:
+        pesquisar_contato()
+    elif selecao == 6:
+        pesquisar_todos_contatos()
+    else:
+        print("Saindo do programa... OBRIGADO!")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# https://www.youtube.com/watch?v=gyqGt20hiI8
+menu()
